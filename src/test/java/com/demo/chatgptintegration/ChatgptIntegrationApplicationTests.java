90285f3b34325a4f6cf0d2572ae3bc97d1a93e20
viveksoni100 <viveksoni100@gmail.com>
2023-10-14 13:04:57 +0530

    chatgpt integration project initiated

---
+++ src/test/java/com/demo/chatgptintegration/ChatgptIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.chatgptintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
