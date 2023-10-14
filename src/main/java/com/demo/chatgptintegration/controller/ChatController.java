d8b4d4cdfd7842f29b2f262d7db8f0d62768668b
viveksoni100 <viveksoni100@gmail.com>
2023-10-14 16:54:02 +0530

    .

--- src/main/java/com/demo/chatgptintegration/controller/ChatController.java
+++ src/main/java/com/demo/chatgptintegration/controller/ChatController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author viveksoni
+ */
+
 @RestController
 public class ChatController {
 
